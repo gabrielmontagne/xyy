e13d0aa6ed11aaaf499f8998ff1392051e3a48f8
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2021-12-25 18:38:08 +0100

    load image from remote file

--- xyy/__init__.py
+++ xyy/__init__.py
@@ -1,5 +1,28 @@
+from IPython import embed
+from flask import Flask, request
+from imageio import read
 import numpy as np
-import bpy
+
+app = Flask(__name__)
+
+def process_frame(func):
+    print('wrap process frame func', func, __name__)
+
+    @app.route('/process', methods=['POST'])
+    def wrapper():
+        print('running nested funcs', request.json)
+        # embed()
+        img = read(request.get('image_path'))
+
+        func(img, request)
+
+        return 'ok', 200
+
+    return wrapper
+
+def start_server(port=3000):
+    app.run(host='0.0.0.0', port=port, threaded=False)
+    print('start XYY server', port)
 
 def label(pixels, text, pos=(10, 25), colour=(255, 255, 255)):
     import cv2
@@ -13,6 +36,7 @@ def join_topbottom(a, b):
     return np.concatenate((a, b))
 
 def prop(name, frame_number, default):
+    import bpy
     world = bpy.context.scene.world
 
     fcurve = None
@@ -26,23 +50,23 @@ def prop(name, frame_number, default):
 
     return result
 
-def process_frame(orig, frame_number, process_name, is_topbottom=False):
-    if is_topbottom:
-        a, b = split_topbottom(orig)
+ #  def process_frame(orig, frame_number, process_name, is_topbottom=False):
+ #      if is_topbottom:
+ #          a, b = split_topbottom(orig)
 
-        a.sort(axis=0)
-        b.sort(axis=0)
+ #          a.sort(axis=0)
+ #          b.sort(axis=0)
 
-        label(a, 'a:{}'.format(frame_number))
-        label(b, 'x:{}'.format(frame_number))
+ #          label(a, 'a:{}'.format(frame_number))
+ #          label(b, 'x:{}'.format(frame_number))
 
-        return join_topbottom(a, b)
+ #          return join_topbottom(a, b)
 
-    orig.sort(axis=0)
-    label(orig, 'x:{}'.format(frame_number))
-    return orig
+ #      orig.sort(axis=0)
+ #      label(orig, 'x:{}'.format(frame_number))
+ #      return orig
 
 
 
 def algo():
-    print('algo xnix')
+    print('algo xyy')
